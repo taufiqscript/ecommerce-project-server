fca55341741847f3ee18b06bc49572323f2b53a2
unknown <28.taufiqurrahman@gmail.com>
2025-09-29 17:35:44 +0700

    update callbackURL in route

--- routes/index.route.js
+++ routes/index.route.js
@@ -11,7 +11,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "ecommerce-project-server-beige.vercel.app/auth/google/callback"
+            callbackURL: "https://ecommerce-project-server-beige.vercel.app/auth/google/callback"
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
