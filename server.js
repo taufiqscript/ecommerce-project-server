5b513fc187cb76a96a42f4bcc74505d8662f137e
unknown <28.taufiqurrahman@gmail.com>
2025-09-29 16:48:17 +0700

    update vercel.json

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const routes = require('./routes/index.route')
 const passport = require('passport')
+
 const { MONGO_URL } = process.env
 
 const app = express()
@@ -11,8 +12,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(passport.initialize())
-
-const PORT = 5000
 
 
 mongoose.connect(MONGO_URL).catch(err => {
@@ -26,6 +25,4 @@ mongoose.connect(MONGO_URL).catch(err => {
 
 app.use(routes)
 
-app.listen(PORT, () => {
-    console.log(`server berjalan di port ${PORT}`)
-})
+module.exports = app
