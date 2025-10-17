16663d8a0e7b7467bd60e7911c71ccc6a56e7b06
unknown <28.taufiqurrahman@gmail.com>
2025-10-17 15:44:24 +0700

    updated code backend

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -16,8 +16,6 @@ const createPayment = async (req, res) => {
 
         const orderId = 'ORDER-' + Date.now()
 
-        const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
-
         const parameter = {
             transaction_details: {
                 order_id: orderId,
@@ -26,9 +24,6 @@ const createPayment = async (req, res) => {
             customer_details: {
                 first_name: customerName,
                 email
-            },
-            callbacks: {
-                finish: `${FRONTEND_URL}/payment-success`
             }
         }
 
